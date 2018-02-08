dddd249faf55e904a5dd7448c79d73be5e3b4e8a
vliang <vghliang@gmail.com>
2018-02-07 19:37:14 -0500

    Adding HTTPmethod selections - Update Specs

--- src/test/java/com/prototype/restclient/service/ClientServiceSpec.groovy
+++ src/test/java/com/prototype/restclient/service/ClientServiceSpec.groovy
@@ -3,6 +3,12 @@ package com.prototype.restclient.service
 import com.prototype.restclient.model.Request
 import com.prototype.restclient.model.Response
 import com.prototype.restclient.service.impl.ClientServiceImpl
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
@@ -17,15 +23,17 @@ class ClientServiceSpec extends Specification {
         given:
         String url = "test_url"
         Request request = new Request(
-                url: "test_url"
+                url: "test_url",
+                httpMethod: HttpMethod.GET
         )
         String body = "test_body"
+        ResponseEntity<String> responseEntity = new ResponseEntity(body, HttpStatus.OK);
 
         when:
         Response response = clientService.process(request)
 
         then:
-        1 * restTemplate.getForObject(url, String.class) >> body
+        1 * restTemplate.exchange(url, HttpMethod.GET, _ as HttpEntity, String.class) >> responseEntity
         0 * _
 
         and:
